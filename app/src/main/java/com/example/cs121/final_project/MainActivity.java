3016fb08b99efa843ed9ee2e97c9cda9e5f4e08a
Taoh Green <greenmachine777@gmail.com>
2016-03-03 12:18:16 -0800

    git ignore

---
+++ app/src/main/java/com/example/cs121/final_project/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.cs121.final_project;
+
+import android.app.TabActivity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TabHost;
+
+public class MainActivity extends TabActivity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // create the TabHost that will contain the Tabs
+        TabHost tabHost = (TabHost)findViewById(android.R.id.tabhost);
+
+
+        TabHost.TabSpec tab1 = tabHost.newTabSpec("First Tab");
+        TabHost.TabSpec tab2 = tabHost.newTabSpec("Second Tab");
+        TabHost.TabSpec tab3 = tabHost.newTabSpec("Third tab");
+        TabHost.TabSpec tab4 = tabHost.newTabSpec("Fourth tab");
+
+        // Set the Tab name and Activity
+        // that will be opened when particular Tab will be selected
+        tab1.setIndicator("Fermentables");
+        tab1.setContent(new Intent(this,Tab1Activity.class));
+
+        tab2.setIndicator("Hops");
+        tab2.setContent(new Intent(this,Tab2Activity.class));
+
+        tab3.setIndicator("Yeast");
+        tab3.setContent(new Intent(this,Tab3Activity.class));
+
+        tab4.setIndicator("Other Ingredients");
+        tab4.setContent(new Intent(this,Tab4Activity.class));
+
+        /** Add the tabs  to the TabHost to display. */
+        tabHost.addTab(tab1);
+        tabHost.addTab(tab2);
+        tabHost.addTab(tab3);
+        tabHost.addTab(tab4);
+
+    }
+}
---
+++ app/src/main/java/com/example/cs121/final_project/Tab3Activity.java
@@ -0,0 +1,13 @@
+package com.example.cs121.final_project;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Tab3Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tab3);
+    }
+}
