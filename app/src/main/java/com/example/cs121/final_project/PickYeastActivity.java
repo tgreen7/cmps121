00faebef7c844c2815dd0fc85d34adeb42578611
Jake Berrier <Halsifer@eduroam-169-233-215-117.ucsc.edu>
2016-03-07 20:51:58 -0800

    Changes the Theme to all activities. #stylin

--- app/src/main/java/com/example/cs121/final_project/PickYeastActivity.java
+++ app/src/main/java/com/example/cs121/final_project/PickYeastActivity.java
@@ -1,13 +1,15 @@
 package com.example.cs121.final_project;
 
+import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ListView;
 
+import java.io.IOException;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-import static com.example.cs121.final_project.Constant.FIFTH_COLUMN;
 import static com.example.cs121.final_project.Constant.FIRST_COLUMN;
 import static com.example.cs121.final_project.Constant.FOURTH_COLUMN;
 import static com.example.cs121.final_project.Constant.SECOND_COLUMN;
@@ -16,11 +18,25 @@ import static com.example.cs121.final_project.Constant.THIRD_COLUMN;
 public class PickYeastActivity extends AppCompatActivity {
 
     private ArrayList<HashMap> list;
+    String[] YeastType = {"", "Ale", "Lager", "Wine", "Champagne"};
+    String[] YeastForm = {"", "Liquid", "Dry"};
+    DataBaseHelper myDbHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pick_yeast);
+        myDbHelper = new DataBaseHelper(this);
+        try {
+            myDbHelper.createDataBase();
+        } catch (IOException ioe) {
+            throw new Error("Unable to create database");
+        }
+        try {
+            myDbHelper.openDataBase();
+        }catch(SQLException sqle){
+            sqle.printStackTrace();
+        }
 
         ListView lview = (ListView) findViewById(R.id.yeast_list);
 
@@ -30,32 +46,21 @@ public class PickYeastActivity extends AppCompatActivity {
     }
 
     private void populateList() {
-
         list = new ArrayList<HashMap>();
+        Cursor cursor = myDbHelper.getReadableDatabase().query("Yeast", null, null, null, null, null, null);
+        cursor.moveToFirst();
 
-        HashMap temp = new HashMap();
-        temp.put(FIRST_COLUMN, "American Ale");
-        temp.put(SECOND_COLUMN, "foiwjfawf");
-        temp.put(THIRD_COLUMN, "Rsawfojaw. 200");
-        temp.put(FOURTH_COLUMN, ". 200");
-        temp.put(FIFTH_COLUMN, "fojaw. 200");
-        list.add(temp);
+        while(!cursor.isAfterLast()){
+            HashMap temp = new HashMap();
+            temp.put(FIRST_COLUMN, cursor.getString(1));
+            temp.put(SECOND_COLUMN, cursor.getString(2));
+            temp.put(THIRD_COLUMN, YeastType[cursor.getInt(3)]);
+            temp.put(FOURTH_COLUMN, YeastForm[cursor.getInt(4)]);
+            list.add(temp);
+            cursor.moveToNext();
+        }
 
-        HashMap temp1 = new HashMap();
-        temp1.put(FIRST_COLUMN,"American Ale 2");
-        temp1.put(SECOND_COLUMN, "afwjoawjf");
-        temp1.put(THIRD_COLUMN, "fwajefoa");
-        temp1.put(FOURTH_COLUMN, "lager");
-        temp1.put(FIFTH_COLUMN, "liquid");
-        list.add(temp1);
-
-        HashMap temp2 = new HashMap();
-        temp2.put(FIRST_COLUMN,"American Ale 2");
-        temp2.put(SECOND_COLUMN, "Bafwjy");
-        temp2.put(THIRD_COLUMN, "oio");
-        temp2.put(FOURTH_COLUMN, "Rsa. 200");
-        temp2.put(FIFTH_COLUMN, "Rsa. 200");
-        list.add(temp2);
+        myDbHelper.close();
 
     }
 }
