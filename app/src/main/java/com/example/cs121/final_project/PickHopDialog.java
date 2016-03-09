11a70d6a3f60d2b7864a5169aea17f8863511979
Taoh Green <greenmachine777@gmail.com>
2016-03-08 17:24:05 -0800

    fixed styling

--- app/src/main/java/com/example/cs121/final_project/PickHopDialog.java
+++ app/src/main/java/com/example/cs121/final_project/PickHopDialog.java
@@ -77,7 +77,7 @@ public class PickHopDialog extends Dialog implements
         Spinner spinner = (Spinner) findViewById(R.id.weight_type);
 // Create an ArrayAdapter using the string array and a default spinner layout
         ArrayAdapter<CharSequence> spin_adapter = ArrayAdapter.createFromResource(getContext(),
-                R.array.weight_type_array, android.R.layout.simple_spinner_item);
+                R.array.weight_type_array, R.layout.my_spinner_text);
 // Specify the layout to use when the list of choices appears
         spin_adapter.setDropDownViewResource(R.layout.my_spinner_style);
 // Apply the adapter to the spinner
@@ -92,6 +92,10 @@ public class PickHopDialog extends Dialog implements
             time.setText("days");
             boil.setText("");
             wort.setChecked(false);
+        }
+        else {
+            time.setText("min");
+            boil.setText("Boil Time");
         }
     }
 
