f8a01d107431423a2d9f9042a08100fb06db8abc
Taoh Green <greenmachine777@gmail.com>
2016-03-11 16:46:31 -0800

    added tabs

--- app/src/main/java/com/example/cs121/final_project/MainActivity.java
+++ app/src/main/java/com/example/cs121/final_project/MainActivity.java
@@ -1,12 +1,9 @@
 package com.example.cs121.final_project;
 
-import android.app.Activity;
 import android.app.TabActivity;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.os.Handler;
-import android.widget.ImageView;
 import android.widget.TabHost;
 import android.widget.TextView;
 
@@ -15,28 +12,59 @@ import com.example.cs121.final_project.Add_Ing_Activities_Dialogs.PickHopActivit
 import com.example.cs121.final_project.Add_Ing_Activities_Dialogs.PickMiscActivity;
 import com.example.cs121.final_project.Add_Ing_Activities_Dialogs.PickYeastActivity;
 
-public class MainActivity extends Activity
+public class MainActivity extends TabActivity
 {
     /** Called when the activity is first created. */
-    /** Duration of wait **/
-    private final int SPLASH_DISPLAY_LENGTH = 1000;
-
-    /** Called when the activity is first created. */
     @Override
-    public void onCreate(Bundle icicle) {
-        super.onCreate(icicle);
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        /* New Handler to start the Menu-Activity
-         * and close this Splash-Screen after some seconds.*/
-        new Handler().postDelayed(new Runnable() {
+        // create the TabHost that will contain the Tabs
+        final TabHost tabHost = (TabHost) findViewById(android.R.id.tabhost);
+
+
+
+        TabHost.TabSpec tab1 = tabHost.newTabSpec("First Tab");
+        TabHost.TabSpec tab2 = tabHost.newTabSpec("Second Tab");
+
+        // Set the Tab name and Activity
+        // that will be opened when particular Tab will be selected
+        tab1.setIndicator("Recipe");
+        tab1.setContent(new Intent(this, Tab1Activity.class));
+
+        tab2.setIndicator("select grain");
+        tab2.setContent(new Intent(this, PickGrainActivity.class));
+
+        /** Add the tabs  to the TabHost to display. */
+        tabHost.addTab(tab1);
+        tabHost.addTab(tab2);
+
+        tabHost.getTabWidget().getChildAt(0).getLayoutParams().height = 80;
+        tabHost.getTabWidget().getChildAt(1).getLayoutParams().height = 80;
+
+        tabHost.getTabWidget().setDividerDrawable(null);
+
+
+        for(int i=0;i<tabHost.getTabWidget().getChildCount();i++) {
+            TextView tv = (TextView) tabHost.getTabWidget().getChildAt(i).findViewById(android.R.id.title); //Unselected Tabs
+            tv.setTextColor(Color.parseColor("#ffffff"));
+            tabHost.getTabWidget().getChildAt(i).setBackgroundColor(getResources().getColor(R.color.light_red));
+        }
+
+
+        tabHost.getTabWidget().getChildAt(tabHost.getCurrentTab()).setBackgroundColor(getResources().getColor(R.color.gold));
+
+        tabHost.setOnTabChangedListener(new TabHost.OnTabChangeListener() {
             @Override
-            public void run() {
-                /* Create an Intent that will start the Menu-Activity. */
-                Intent mainIntent = new Intent(MainActivity.this, Tab1Activity.class);
-                MainActivity.this.startActivity(mainIntent);
-                MainActivity.this.finish();
+            public void onTabChanged(String arg0) {
+                for(int i=0;i<tabHost.getTabWidget().getChildCount();i++) {
+                    tabHost.getTabWidget().getChildAt(i).setBackgroundColor(getResources().getColor(R.color.light_red));
+                }
+                tabHost.getTabWidget().getChildAt(tabHost.getCurrentTab()).setBackgroundColor(getResources().getColor(R.color.gold));
             }
-        }, SPLASH_DISPLAY_LENGTH);
+        });
     }
+
+
 }
